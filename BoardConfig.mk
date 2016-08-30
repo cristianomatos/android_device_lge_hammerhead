7737d318f3164259204c6af8a17d195df66b349a
cristianomatos <cristianobmatos@gmail.com>
2016-08-29 21:11:34 -0300

    Inline kernel building using CM kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,9 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 734003200
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
+# Define kernel config for inline building
+TARGET_KERNEL_CONFIG := cyanogenmod_hammerhead_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 
 ifneq ($(filter hammerhead_fp aosp_hammerhead_fp,$(TARGET_PRODUCT)),)
 BOARD_HAS_FINGERPRINT_FPC := true
